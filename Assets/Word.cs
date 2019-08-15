6714b515bb2262c8cd2f95635c08ca67548acd0d
Leonardo Vasques <quenixserra@gmail.com>
2019-08-15 18:38:56 -0300

    detect key input

--- Assets/Word.cs
+++ Assets/Word.cs
@@ -7,9 +7,33 @@ public class Word
 {
 
     public string word;
+    private int typeIndex;
 
     public Word (string _word)
     {
         word = _word;
+        typeIndex = 0;
+    }
+
+    public char GetNextLetter ()
+    {
+        return word[typeIndex];
+    }
+
+    public void TypeLetter ()
+    {
+        typeIndex++;
+        //Remove the letter on screen
+    }
+
+    public bool WordTyped ()
+    {
+        bool wordTyped = (typeIndex >= word.Length);
+        if (wordTyped)
+        {
+            //Remove the word on screen
+        }
+
+        return wordTyped;
     }
 }
---
+++ Assets/WordInput.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordInput : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (char letter in Input.inputString)
+        {
+            Debug.Log(letter);
+        }       
+    }
+}
