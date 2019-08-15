55256866f33cb9095f88b0d341cccaf76c6d464c
Leonardo Vasques <quenixserra@gmail.com>
2019-08-15 05:41:53 -0300

    Rascunho do projeto

---
+++ Assets/WordGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour
+{
+
+   private static string[] wordList = {"word1", "word2", "word3", "word4", "word5"};
+
+   public static string GetRandomWord ()
+   {
+
+       int randomIndex = Random.Range(0, wordList.Length);
+       string randomWord = wordList[randomIndex];
+
+       return randomWord;
+
+   }
+}
---
+++ Assets/WordManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordManager : MonoBehaviour
+{
+    public List<Word> words;
+
+    private void Start()
+    {
+        AddWord();
+        AddWord();
+        AddWord();
+
+    }
+
+    public void AddWord()
+    {
+        Word word = new Word(WordGenerator.GetRandomWord());
+        Debug.Log(word.word);
+    }
+}
