7cacc9168bf67efb621302c5695d6ac684e274e3
Leonardo Vasques <quenixserra@gmail.com>
2019-08-21 03:20:52 -0300

    Background music

---
+++ Assets/BGM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    private static BGM instance = null;
+    private static BGM Instance
+    {
+        get { return instance; }
+    }
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
